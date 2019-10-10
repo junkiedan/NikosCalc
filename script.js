a565e5907a61fe701d62a327ce6ce5bde69b6fab
thestbar <stavrosbarousis@gmail.com>
2019-10-10 10:56:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById("result").addEventListener("click", function(){
     var x = parseFloat(document.getElementById("xNum").value);
     var y = parseFloat(document.getElementById("yNum").value);
     var z = parseFloat(document.getElementById("zNum").value);
-    var res = 0.01*x + 0.01*y + 0.01*z;
-    var fixedRes = parseFloat(Math.round(res * 100) / 100).toFixed(2);
-    document.getElementById("resultText").innerHTML = fixedRes;
+    var res = (100/x) + (100/y) + (100/z);
+    // var fixedRes = parseFloat(Math.round(res * 100) / 100).toFixed(2);
+    document.getElementById("resultText").innerHTML = res;
     // document.getElementById("test").innerHTML = res;
 });
 
@@ -14,9 +14,9 @@ document.body.addEventListener("keypress", function (e) {
         var x = parseFloat(document.getElementById("xNum").value);
         var y = parseFloat(document.getElementById("yNum").value);
         var z = parseFloat(document.getElementById("zNum").value);
-        var res = 0.01*x + 0.01*y + 0.01*z;
-        var fixedRes = parseFloat(Math.round(res * 100) / 100).toFixed(2);
-        document.getElementById("resultText").innerHTML = fixedRes;
+        var res = (100/x) + (100/y) + (100/z);
+        // var fixedRes = parseFloat(Math.round(res * 100) / 100).toFixed(2);
+        document.getElementById("resultText").innerHTML = res;
         // document.getElementById("test").innerHTML = res;
     }
 });
